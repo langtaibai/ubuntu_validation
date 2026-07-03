c7c855e1e91aaa4e063bb41e8f784461e0bb7ce9
langtaibai <745783778@qq.com>
2026-07-03 14:09:34 +0800

    validation layer

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
-from tests.test_system import TestSystem
+from tests.boot_test import BootTest
+#from tests.test_system import TestSystem
 
 def main():
-    tester = TestSystem()
-    tester.connect()
-    tester.check_system_status()
-    tester.collect_journal()
-    tester.disconnect()
+    # tester = TestSystem()
+    # tester.connect()
+    # tester.check_system_status()
+    # tester.collect_journal()
+    # tester.disconnect
+    boot = BootTest()
+    boot.connect()
+    result= boot.run()
+    boot.disconnect()
+    print(result)
 if __name__ == "__main__":
     main()
---
+++ tests/boot_test.py
@@ -0,0 +1,43 @@
+from core.ssh_client import SSHClient
+from config.config import load_config
+
+class BootTest:
+    def __init__(self):
+        config = load_config()
+        self.ssh = SSHClient(
+            host=config["host"],
+            user=config["user"],
+            password=config["password"]
+        )
+
+    def connect(self):
+        """connect test machine"""
+        self.ssh.connect()
+
+    def disconnect(self):
+        """disconnect test machine"""
+        self.ssh.close()
+
+    def run(self):
+        result = self.ssh.run(
+            "systemctl is-system-running"
+        )
+        status = result["stdout"].strip()
+        if status == "running":
+            return {
+                "name": "Boot Test",
+                "result": "PASS",
+                "message": "System is running"
+            }
+        elif status == "degraded":
+            return {
+                "name": "Boot Test",
+                "result": "WARNING",
+                "message": "System is degraded"
+            }
+        else:
+            return {
+                "name": "Boot Test",
+                "result": "FAIL",
+                "message": "System is not running"
+            }
